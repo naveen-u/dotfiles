51fd114d108ddb272fb2fa63942f56de8d6d9bbf
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2025-05-06 12:04:07 -0700

    [nvim] Enable listchars for tabs and trailing spaces. Update plugins.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.list = true     -- show certain non-printable characters
+vim.opt.listchars = {   -- update listchars for tabs and trailing spaces
+  tab = ">~",
+  trail = "·",
+}
