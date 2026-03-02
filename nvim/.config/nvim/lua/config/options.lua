fb254d1f9edc1319e18b4dc8941592bd95885174
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2026-03-01 20:21:52 -0800

    [nvim] Delete existing lazy nvim config

--- nvim/.config/nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.opt.list = true     -- show certain non-printable characters
-vim.opt.listchars = {   -- update listchars for tabs and trailing spaces
-  tab = ">~",
-  trail = "·",
-}
