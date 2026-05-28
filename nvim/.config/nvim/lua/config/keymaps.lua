260b8ba8195fddb7042d0028a2da6db73f3fa020
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2026-05-27 17:15:43 -0700

    [nvim] Add keymaps for pane management

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -16,6 +16,18 @@ vim.keymap.set("n", "<leader>tl", "<cmd>tabnext<CR>", { desc = "Tab next" })
 vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "Tab new" })
 vim.keymap.set("n", "<leader>to", "<cmd>tabonly<CR>", { desc = "Tab only" })
 
+-- Panes
+vim.keymap.set("n", "<Leader>wv", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<Leader>wh", "<cmd>split<CR>", { desc = "Horizontal split" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Switch to left pane" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Switch to bottom pane" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Switch to top pane" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Switch to right pane" })
+vim.keymap.set("n", "<C-A-h>", "<cmd>vertical resize -5<CR>", { desc = "Resize pane left (shrink)" })
+vim.keymap.set("n", "<C-A-l>", "<cmd>vertical resize +5<CR>", { desc = "Resize pane right (grow)" })
+vim.keymap.set("n", "<C-A-j>", "<cmd>horizontal resize -5<CR>", { desc = "Resize pane down (shrink)" })
+vim.keymap.set("n", "<C-A-k>", "<cmd>horizontal resize +5<CR>", { desc = "Resize pane up (grow)" })
+
 -- Yank into system clipboard
 vim.keymap.set({ "n", "v" }, "<C-c>", [["+y]])
 
