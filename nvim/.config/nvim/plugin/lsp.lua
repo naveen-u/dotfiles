23713baa953e810255ce0dd96a16d0ffa201b27a
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2026-05-28 20:14:43 -0700

    [nvim] Add LSP keymaps

--- nvim/.config/nvim/plugin/lsp.lua
+++ nvim/.config/nvim/plugin/lsp.lua
@@ -34,3 +34,35 @@ vim.lsp.enable({
 	"texlab",
 	"clangd",
 })
+
+-- Set up LSP keymaps when LSP attaches to a buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+	callback = function(event)
+		local bufnr = event.buf
+
+		vim.keymap.set("n", "gd", function()
+			vim.lsp.buf.definition()
+		end, { buffer = bufnr, remap = false, desc = "Goto definition" })
+
+		vim.keymap.set("n", "gD", function()
+			vim.lsp.buf.declaration()
+		end, { buffer = bufnr, remap = false, desc = "Goto declaration" })
+
+		vim.keymap.set("n", "ge", function()
+			vim.diagnostic.jump({ count = 1, float = true })
+		end, { buffer = bufnr, remap = false, desc = "Goto next error" })
+
+		vim.keymap.set("n", "gE", function()
+			vim.diagnostic.jump({ count = -1, float = true })
+		end, { buffer = bufnr, remap = false, desc = "Goto prev error" })
+
+		vim.keymap.set("n", "<leader>vd", function()
+			vim.diagnostic.open_float()
+		end, { buffer = bufnr, remap = false, desc = "View diagnostics" })
+
+		vim.keymap.set("n", "<leader>vh", function()
+			vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+		end, { buffer = bufnr, remap = false, desc = "Toggle inlay hints" })
+	end,
+})
